6f8e42172bedf42cf8db97ef9af9e5c7069c99ed
Quintin H <qharter@gmail.com>
2021-02-03 21:26:54 -0500

    initial project

---
+++ AxoManager/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AxoManager.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
