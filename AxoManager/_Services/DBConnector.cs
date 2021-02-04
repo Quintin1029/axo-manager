e8406edde11e2307cc3f4841d84a2a02ad5a5e7c
Quintin H <qharter@gmail.com>
2021-02-03 22:33:49 -0500

    Start of DB stuff

---
+++ AxoManager/_Services/DBConnector.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.Data.Sqlite;
+
+public class DBConnector
+{
+
+    private string _query;
+
+	public DBConnector(string query)
+	{
+        _query = query;
+    }
+
+    public Object ExecuteScalar()
+    {
+        var connectionStringBuilder = new SqliteConnectionStringBuilder();
+        connectionStringBuilder.DataSource = Library.DB_ADDR; //May need to change this later
+
+        using (var connection = new SqliteConnection(connectionStringBuilder.ConnectionString))
+        {
+            connection.Open();
+
+            Object ret = null;
+
+            using (var transaction = connection.BeginTransaction())
+            {
+                var insertCmd = connection.CreateCommand();
+
+                insertCmd.CommandText = _query;
+                ret = insertCmd.ExecuteScalar();
+                transaction.Commit();
+            }
+
+            return ret;
+        }
+    }
+}
---
+++ AxoManager/_Services/Library.cs
@@ -0,0 +1,6 @@
+﻿using System;
+
+public static class Library
+{
+	public static string DB_ADDR = "C:\\Users\\qhart\\db\\AxoManagerDB.db";
+}
