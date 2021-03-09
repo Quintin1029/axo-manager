b43474930f7dcf8787fa69c1c2e1d96c1f62f87e
Erin Lincoln <el77717@gmail.com>
2021-03-08 19:11:47 -0500

    Added Recruitment tab

--- AxoManager/Controllers/HomeController.cs
+++ AxoManager/Controllers/HomeController.cs
@@ -30,6 +30,11 @@ namespace AxoManager.Controllers
             return View();
         }
 
+        public IActionResult Recruitment()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
